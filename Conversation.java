4f5ecb5113076ec5f2248cbcd869c538021e8647
MichelleXu00 <mxu32@smith.edu>
2025-02-16 17:12:18 -0500

    attempt 1

--- Conversation.java
+++ Conversation.java
@@ -1,21 +1,45 @@
+import java.util.Scanner;
 
 class Conversation implements Chatbot {
 
   // Attributes 
+  int numrounds;
+  String inputString;
 
   /**
    * Constructor 
    */
-  Conversation() {
-    
+  public Conversation() {
+
   }
+
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
 
-  }
+    // Print welcome and ask for the number of rounds
+    System.out.println("Welcome to Java Conversation");
+    System.out.println("How many rounds?");
+    
+    //get number of rounds
+    Scanner input = new Scanner(System.in);
+    int numrounds = input.nextInt();
+    System.out.println(numrounds);
+    System.out.println("Hi there! What's on your mind?");
+
+    //start the conversation
+    for (int i = 0; i < numrounds; i++) {
+      String inputString = input.nextLine();
+      System.out.println(respond(inputString));
+      }
+
+      System.out.println("See ya!");
+      input.close();
+    }
+    
+
 
   /**
    * Prints transcript of conversation
@@ -30,8 +54,36 @@ class Conversation implements Chatbot {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    String returnString = inputString; 
+    if (inputString.contains(".")) {
+      returnString =  inputString.replace(".", "?");
+    }
+    if (inputString.contains("I am")) {
+      returnString =  inputString.replace("I am", "Are you");
+    } if (inputString.contains("I")) {
+      returnString =  inputString.replace("I", "you");
+    }
+    if (inputString.contains("me")) {
+      returnString =  inputString.replace("me", "you");
+    }
+    if (inputString.contains("am")) {
+      returnString =  inputString.replace("am", "are");
+    }
+    if (inputString.contains("you")) {
+      returnString =  inputString.replace("you", "I");
+    }
+    if (inputString.contains("my")) {
+      returnString =  inputString.replace("my", "your");
+    }
+    if (inputString.contains("your")) {
+      returnString =  inputString.replace("your", "my");
+    }
+    if (!(inputString.contains("I") || inputString.contains("me") || 
+      inputString.contains("am") || inputString.contains("you") || 
+      inputString.contains("my") || inputString.contains("your"))) {
+      returnString = "Mmm-hm.";
+    return returnString;
+    }
   }
 
   public static void main(String[] arguments) {
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I talked to Hanyu and Rainbow while working on this assignment. 
+
 - Which session(s) of TA / office hours did you attend?
+I attended the TA hours on Sunday afternoon and asked for help from Zihan.
+
 - What are your initial impressions of Java? 
+I feel very frustrated about Java now. It is difficult to switch from Python to Java since there are a lot of differences and I cannot use pythontutor to check my code line by line. 
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+The data types are the same in both languages. They both have loops, if statements, and return. The way of implement the ideas, the grammar, of the languages are different. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I don't know what to put in the constructor and printtranscript method. I wrote a basic frame for the chat method and respond method. I have an error in the respond method that I don't know how to fix. Try to look up syntax for loop and if statement for java online. 
